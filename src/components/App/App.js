be5c9e314844ecbfb260299994a2cadc1e813940
paulo4g <heypaulim@gmail.com>
2022-06-02 13:50:20 -0300

    teste

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SmartCounter from '../SmartCounter/SmartCounter';
+
+function App() {
+  return (
+    <>
+      <h1>Olá, mundo</h1>
+      <SmartCounter />
+    </>
+  )
+}
+
+export default App;
